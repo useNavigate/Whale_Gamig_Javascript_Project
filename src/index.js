bbd2e6ee86c93600339b613f4f713587a84b6659
useNavigate <useNavigate@gmail.com>
2023-03-09 23:04:48 -0500

    render simple html with fetched data

--- src/index.js
+++ src/index.js
@@ -9,8 +9,10 @@ document.addEventListener("DOMContentLoaded", () => {
   //grab an html element for DOM manipulation
   const main = document.getElementById("main");
   //   new Example(main);
-
+//  handleButtonClick(); can add all the eventlistner like this
+  //need to make some html for navbar and search section so we can save it as variable to have handle events
   useData().then((data) => {
+
     console.log(data.allData[0].title)
     //i need to decide how i am going to render this
      handleMainPage(data.allData[0].title, main);
