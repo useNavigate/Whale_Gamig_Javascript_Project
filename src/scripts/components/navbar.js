365d621064f4b9c4e8b425f85b61cec4a9b40f4e
useNavigate <useNavigate@gmail.com>
2023-03-12 19:25:29 -0400

    links!

--- src/scripts/components/navbar.js
+++ src/scripts/components/navbar.js
@@ -2,7 +2,7 @@ export const renderNavbar = () => {
 
     const navBar = document.getElementById('menu')
     navBar.innerHTML = `
-    <li><a href="https://usenavigate.github.io/Whale_Gaming_Javascript_Project/"><h1>🐳</h1></a></li>
+    <li><a href="https://usenavigate.github.io/Whale_Gaming_Javascript_Project/"><h1>🐳🐋</h1></a></li>
     <li id="game_menu">Games</li>
     <li id="profile_menu">Profile</li>
     <li id="stat_menu">statistics</li>
