3ccdc0abfc74d2dd2e87dfe45f0711d81d4c92e4
useNavigate <useNavigate@gmail.com>
2023-03-14 15:04:16 -0400

    now its linked to navbar as well

--- src/scripts/components/mainPage.js
+++ src/scripts/components/mainPage.js
@@ -36,9 +36,8 @@ export function handlePage(allData) {
     main.innerHTML = "";
     main.classList.add("card_page");
     allData.forEach((game) => {
-
       const card = document.createElement("div");
-         card.innerHTML = `
+      card.innerHTML = `
 <div class="card" style="background-image: url('${game.thumbnail}'); background-size: cover;">
       <ul  class="card_info">
       <li><h1 id ="game_title">${game.title}</h1></li>
@@ -50,16 +49,27 @@ export function handlePage(allData) {
       `;
       main.appendChild(card);
 
+      const button = document.getElementById(game.id);
+      button.addEventListener("click", (event) => {
+        console.log(event.target.id);
+        /// now it needs to fetch the data based on the id
+        fetch(`https://mmo-games.p.rapidapi.com/game?id=${event.target.id}`, {
+          method: "GET",
+          headers: {
+            "X-RapidAPI-Key":
+              "5baae6da7amsh6d563cbf11ac2a0p1d6d56jsn48853ab50d67",
+            "X-RapidAPI-Host": "mmo-games.p.rapidapi.com",
+          },
+        })
+          .then((res) => res.json())
+          .then((data) => console.log(data))
+          .catch((err) => console.error("error:" + err));
+      });
+    });
 
- const button = document.getElementById(game.id);
- button.addEventListener("click", (event) => {
-   console.log(event.target.id);
  });
 
 
-
-    });
-  });
 
 
 }
--- src/scripts/components/navbar.js
+++ src/scripts/components/navbar.js
@@ -32,16 +32,35 @@ export function handleNavbarClick(allData, data) {
  allData.forEach((game) => {
    const card = document.createElement("div");
    main.classList.add('card_page')
-   card.innerHTML = `
+  card.innerHTML = `
 <div class="card" style="background-image: url('${game.thumbnail}'); background-size: cover;">
-      <ul class="card_info">
+      <ul  class="card_info">
       <li><h1 id ="game_title">${game.title}</h1></li>
       <li class="genre">${game.genre}</li>
       <li class="description">${game.short_description}</li>
+      <button id="${game.id}" class="more">Learn More</button>
       </ul>
       </div>
       `;
-   main.appendChild(card);
+  main.appendChild(card);
+
+      const button = document.getElementById(game.id);
+      button.addEventListener("click", (event) => {
+        console.log(event.target.id);
+        /// now it needs to fetch the data based on the id
+        fetch(`https://mmo-games.p.rapidapi.com/game?id=${event.target.id}`, {
+          method: "GET",
+          headers: {
+            "X-RapidAPI-Key":
+              "5baae6da7amsh6d563cbf11ac2a0p1d6d56jsn48853ab50d67",
+            "X-RapidAPI-Host": "mmo-games.p.rapidapi.com",
+          },
+        })
+          .then((res) => res.json())
+          .then((data) => console.log(data))
+          .catch((err) => console.error("error:" + err));
+      });
+
  })
   })
 }
