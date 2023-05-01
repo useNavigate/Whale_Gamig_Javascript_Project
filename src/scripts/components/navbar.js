44ee9460cd63ad01930b5a4e6b1649ef5313acc8
useNavigate <useNavigate@gmail.com>
2023-05-01 10:43:29 -0400

    fixed styling

--- src/scripts/components/navbar.js
+++ src/scripts/components/navbar.js
@@ -3,41 +3,37 @@ export function scrollToTop() {
   window.scrollTo({ top: 0, behavior: "smooth" });
 }
 export const renderNavbar = () => {
-
-    const navBar = document.getElementById('menu')
-    navBar.innerHTML = `
+  const navBar = document.getElementById("menu");
+  navBar.innerHTML = `
     <li id="home"><a href="https://usenavigate.github.io/Whale_Gaming_Javascript_Project/">Home</a></li>
     <li id="game_menu">Games</li>
     <li id="stat_menu">Statistics</li>
+       <li id="profile_menu"><a href="https://github.com/useNavigate"><i class="fa-brands fa-github"></i></a></li>
+      <li id="profile_menu"><a href="https://www.linkedin.com/in/use-navigate-798165261/"><i
+            class="fa-brands fa-linkedin"></i></a></li>
 
     `;
-
-
 };
 
 export function handleNavbarClick(allData, data) {
   const gameMenu = document.querySelector("#game_menu");
-  const statMenu = document.querySelector("#stat_menu")
+  const statMenu = document.querySelector("#stat_menu");
   statMenu.addEventListener("click", (event) => {
     event.preventDefault();
-
     main.innerHTML = "";
-    // handleFilter(data);
-    createChart(allData,data);
-
-    // createMultiChart(dispatchGenre(data));
+    createChart(allData, data);
   });
 
-  gameMenu.addEventListener("click",event=>{
-    event.preventDefault()
-        main.style.backgroundColor = "#0a1929";
-    main.innerHTML=""
-       main.classList.add("cards");
- allData.forEach((game) => {
-   const card = document.createElement("div");
-   main.classList.add('card_page')
-   //rendering cards
-  card.innerHTML = `
+  gameMenu.addEventListener("click", (event) => {
+    event.preventDefault();
+    main.style.backgroundColor = "#0a1929";
+    main.innerHTML = "";
+    main.classList.add("cards");
+    allData.forEach((game) => {
+      const card = document.createElement("div");
+      main.classList.add("card_page");
+      //rendering cards
+      card.innerHTML = `
 <div class="card" style="background-image: url('${game.thumbnail}'); background-size: cover;">
       <ul  class="card_info">
       <li><h1 id ="game_title">${game.title}</h1></li>
@@ -47,11 +43,10 @@ export function handleNavbarClick(allData, data) {
       </ul>
       </div>
       `;
-  main.appendChild(card);
-scrollToTop();
+      main.appendChild(card);
+      scrollToTop();
       const button = document.getElementById(game.id);
       button.addEventListener("click", (event) => {
-
         console.log(event.target.id);
         /// now it needs to fetch the data based on the id
         fetch(`https://mmo-games.p.rapidapi.com/game?id=${event.target.id}`, {
@@ -63,17 +58,21 @@ scrollToTop();
           },
         })
           .then((res) => res.json())
-          .then((data) => {console.log(data)
-            main.innerHTML=""
-            const detail_section = document.createElement("div")
-             detail_section.classList.add("d_main");
-               main.style.backgroundColor = "#132f4c";
-               const image = data.screenshots.length !==0 ?[
-                 data.screenshots[0].image,
-                 data.screenshots[1].image,
-                 data.screenshots[2].image,
-               ] : "Screenshot Unavailable ";
-   detail_section.innerHTML = `
+          .then((data) => {
+            console.log(data);
+            main.innerHTML = "";
+            const detail_section = document.createElement("div");
+            detail_section.classList.add("d_main");
+            main.style.backgroundColor = "#132f4c";
+            const image =
+              data.screenshots.length !== 0
+                ? [
+                    data.screenshots[0].image,
+                    data.screenshots[1].image,
+                    data.screenshots[2].image,
+                  ]
+                : "Screenshot Unavailable ";
+            detail_section.innerHTML = `
 
   <div class="d_pictures">
   <h1>${data.title}</h1>
@@ -114,13 +113,11 @@ scrollToTop();
   </div>
 
             `;
-            main.appendChild(detail_section)
-            scrollToTop()
-
+            main.appendChild(detail_section);
+            scrollToTop();
           })
           .catch((err) => console.error("error:" + err));
       });
-
- })
-  })
+    });
+  });
 }
