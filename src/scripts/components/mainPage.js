f6c902fd6f52818c4453fece9b1b12562042d9d1
useNavigate <useNavigate@gmail.com>
2023-05-04 12:25:14 -0400

    target - blank

--- src/scripts/components/mainPage.js
+++ src/scripts/components/mainPage.js
@@ -111,7 +111,7 @@ export function handlePage(allData) {
     <div class="d_bar">
    <h1>Description</h1>
    </div>
-    <div class="d_description">${data.description}   <a id="play" href=${data.game_url}>Play Now</a> </div>
+    <div class="d_description">${data.description}   <a id="play" href=${data.game_url} target="_blank">Play Now</a> </div>
 
 
 
